22dae521792b36976e25370ab438a516b7f4f7e0
Matthew Schembri <matthew.schembri@gainchanger.com>
2023-08-22 16:09:07 +0200

    Create ceaser_code.py

---
+++ Day 8/ceaser_code.py
@@ -0,0 +1,49 @@
+def encrypt(text, shift):
+    cipher_text = ''
+    for letter in text:
+        pos = alphabet.index(letter)
+        new_pos = pos + shift
+        new_letter = alphabet[new_pos]
+        
+        cipher_text += new_letter
+    print(cipher_text)
+    return cipher_text
+
+def decrypt(cipher_text, shift):
+    text = ''
+    for letter in cipher_text:
+        pos = alphabet.index(letter)
+        new_pos = pos - shift
+        new_letter = alphabet[new_pos]
+        
+        text += new_letter
+    print(text)
+    return text
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+if direction is 'e':
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
+
+
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
