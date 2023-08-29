ea3d418ff3808b7c720ecd0b856d504607a461fe
Matthew Schembri <matthew.schembri@gainchanger.com>
2023-08-29 15:01:12 +0200

    day 16 OOP

---
+++ Day 16/main.py
@@ -0,0 +1,29 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+def coffee_machine():
+    money_machine = MoneyMachine()
+    cofee_maker = CoffeeMaker()
+    menu = Menu()
+    is_on = True
+    
+    while is_on:
+        options = menu.get_items()
+        choice = input(f"What would you like? {options}: ")
+        
+        if choice == "off":
+            is_on = False
+        elif choice == "report":
+            cofee_maker.report()
+            money_machine.report()            
+        else:
+            drink = menu.find_drink(choice)
+            
+            if cofee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                cofee_maker.make_coffee(drink)
+            else:
+                print("Insuffucient resources")
+                
+    
+coffee_machine()
