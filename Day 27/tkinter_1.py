ded81693afd3cbdd5d67a98d9d2cb0c271cda142
Matthew Schembri <matthew.schembri@gainchanger.com>
2023-09-18 09:29:11 +0200

    Create tkinter_1.py

---
+++ Day 27/tkinter_1.py
@@ -0,0 +1,14 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("MAMMA MIA")
+window.minsize(width=500, height=300)
+
+label = tkinter.Label(text="I am iron man")
+label.pack()
+
+
+
+
+
+window.mainloop()
